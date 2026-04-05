a838564d7b67ae1e54bc9d828a561c9dd162d529
Vadim Voronov <voronov.vadim@huawei-partners.com>
2026-04-05 20:55:24 +0300

    check

--- frameworks/core/interfaces/native/implementation/canvas_renderer_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/canvas_renderer_accessor.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// check
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
 #include "core/interfaces/native/utility/converter_union.h"
